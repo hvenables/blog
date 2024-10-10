525952749b8a0eb7c1700b2ed769f865fbc1f4d8
Harry Venables <venablesh@yahoo.co.uk>
2024-10-10 10:24:12 +0100

    Add character count to summary

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,8 @@
 import { Application } from "@hotwired/stimulus"
+import CharacterCounter from '@stimulus-components/character-counter'
 
 const application = Application.start()
+application.register('character-counter', CharacterCounter)
 
 // Configure Stimulus development experience
 application.debug = false
