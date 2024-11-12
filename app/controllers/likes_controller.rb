0cc696117afae05d85519daff7d0d9353bc583b8
Harry Venables <hvenables89@gmail.com>
2024-11-12 12:48:08 +0000

    Minor improvements to like controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,18 @@
 class LikesController < ApplicationController
   before_action :set_anonymous_user_id
+  before_action :load_article
 
   def create
-    @article = Article.friendly.find(params[:article_id])
     Like.create!(uuid: cookies[:uuid], article: @article)
   end
 
   def destroy
+    Like.find_by!(uuid: cookies[:uuid], article: @article).destroy!
+  end
+
+  private
+
+  def load_article
     @article = Article.friendly.find(params[:article_id])
-    Like.find_by!(uuid: cookies[:uuid], article: @article).destroy
   end
 end
