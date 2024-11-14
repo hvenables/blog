4cf44b09f85d0a17cdffe3e3f02460d1c6163aa3
Harry Venables <hvenables89@gmail.com>
2024-11-14 15:01:29 +0000

    test removing config

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,20 +1,5 @@
 require "test_helper"
 
-Capybara.register_driver :headless_chrome do |app|
-  options = Selenium::WebDriver::Chrome::Options.new
-  options.add_argument("--headless")                        # Run in headless mode
-  options.add_argument("--no-sandbox")                      # Required for CircleCI
-  options.add_argument("--disable-dev-shm-usage")          # Required for CircleCI
-  options.add_argument("window-size=1400x1400")            # Optional: Ensures proper window size
-  options.add_argument("--remote-debugging-port=9222")     # Optional: Debugging for CI issues
-  options.add_argument("--disable-gpu")                    # Optional: Helps avoid rendering issues
-  options.add_argument("--disable-software-rasterizer")    # Optional
-
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
-end
-
-Capybara.default_driver = :headless_chrome
-
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
 
